eade1fc038d8176f73577e406bbf3448a590ef4f
Metrevelii <72525534+Metrevelii@users.noreply.github.com>
2024-05-08 20:07:57 +0400

    PATH user profiles

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -18,6 +18,15 @@ const usersController = {
         } catch(error) {
             next(error);
         }
+    },
+    async updateProfile(req, res, next) {
+        try {
+            const user = await userService.updateUserProfile(req)
+            res.json(user);
+
+        } catch(error) {
+            next(error);
+        }
     }
 
 }
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,5 +1,6 @@
-const { User } = require("../models/user")
-
+const { User } = require("../models/user");
+const httpStatus = require('http-status');
+const { ApiError } = require('../middleware/apiError');
 
 const findUserByEmail = async(email) => {
     return await User.findOne({email})
@@ -9,7 +10,35 @@ const findUserById = async(_id) => {
     return await User.findById(_id);
 }
 
+const updateUserProfile = async(req) => {
+    try {
+
+        // make sure to validate what you want to patch !!!111111111111123123
+        
+
+        const user = await User.findOneAndUpdate(
+            { _id: req.user._id },
+            {
+                $set: {
+                    ...req.body.data
+                }
+            },
+            { new: true}
+        );
+
+        if (!user) {
+            throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+        }
+
+        return user;
+
+    }catch(error) {
+        throw error;
+    }
+}
+
 module.exports = {
     findUserByEmail,
-    findUserById
+    findUserById,
+    updateUserProfile
 }
