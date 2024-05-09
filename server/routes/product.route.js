e2f35695f90d9b47740b9bd4d026ee470c056251
Metrevelii <72525534+Metrevelii@users.noreply.github.com>
2024-05-09 19:20:17 +0400

    add GET, PATCH and DELETE products functionality

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -6,6 +6,10 @@ const { addProductValidator } = require('../middleware/validations');
 
 router.post('/', auth('createAny','product'), addProductValidator, productsController.addProduct);
 
+router.route('/product/:id')
+.get(productsController.getProductById)
+.patch(auth('updateAny','product'), productsController.updateProductById)
+.delete(auth('deleteAny', 'product'), productsController.deleteProductById);
 
 
 
--- server/services/products.service.js
+++ server/services/products.service.js
@@ -1,19 +1,58 @@
 const { Product } = require("../models/product");
+const { ApiError } = require("../middleware/apiError");
+const httpStatus = require("http-status");
 
+const addProduct = async (body) => {
+  try {
+    const product = new Product({
+      ...body,
+    });
+    await product.save();
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const addProduct = async( body ) => {
+const getProductById = async (_id) => {
+  try {
+    const product = await Product.findById(_id).populate("brand");
+    if (!product) throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateProductById = async (_id, body) => {
+  try {
+    const product = await Product.findOneAndUpdate(
+      { _id },
+      { $set: body },
+      { new: true }
+    );
+
+    if (!product)
+      throw new ApiError(httpStatus.NOT_FOUND, "Product not found.");
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteProductById = async( _id  ) => {
     try {
-        const product = new Product({
-            ...body
-        });
-        await product.save();
-        return product;
-
+        const product = await Product.findByIdAndDelete(_id);
+        if(!product) throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
+        return product
     } catch(error) {
-        throw error;
+        throw error
     }
 }
 
 module.exports = {
-    addProduct
-}
+  addProduct,
+  getProductById,
+  updateProductById,
+  deleteProductById
+};
