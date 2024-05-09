8d17e5544ff5384d34b6986362c17614e9f6d94d
Metrevelii <72525534+Metrevelii@users.noreply.github.com>
2024-05-09 14:51:37 +0400

    POST method -- add brands

---
+++ server/controllers/brand.controller.js
@@ -0,0 +1,17 @@
+const { brandService } = require('../services');
+
+
+const brandControllers = {
+    async addBrand(req,res, next) {
+        try {
+            const brand = await brandService.addBrand(req.body.brandname);
+
+            res.json(brand);
+
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = brandControllers;
