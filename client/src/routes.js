bc1c889784bc8768003c3edaec13767d4c87ee0c
Metrevelii <72525534+Metrevelii@users.noreply.github.com>
2024-05-12 21:09:15 +0400

    starting routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/navigation/header';
+import Footer from './components/navigation/footer';
+import Home from './components/home'
+
 
 
 function App() {
   return (
-    <div className="App">
-        Hi
-    </div>
+    <BrowserRouter className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+        <Footer />
+    </BrowserRouter>
   );
 }
 
