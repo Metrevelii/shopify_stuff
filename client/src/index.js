aac3795fe1bed1cf3ae43cc0ccb1d73bccef3237
Metrevelii <72525534+Metrevelii@users.noreply.github.com>
2024-06-27 22:46:15 +0400

    connecting to redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,16 @@ import Routes from "./routes";
 
 import "./resources/styles/styles.css";
 
+// translator -- will do the connection to redux
+import { Provider } from 'react-redux';
+import ReduxStore from './store';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={ReduxStore()}>
+      <Routes />
+
+    </Provider>
   </React.StrictMode>
 );
